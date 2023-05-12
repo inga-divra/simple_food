a5c16f1871c39adf7db1d03ac37798aa9f0962f5
inga-divra <ingady@gmail.com>
2023-05-12 18:35:20 +0300

    updates files in docs folder

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,18 +1,21 @@
+//reviews section slider
+$(function () {
+  $('.slider').slick({
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: 'cubic-bezier(0.600, -0.280, 0.735, 0.045)',
+  });
+});
+
 //set date in footer
 const date = (document.getElementById('date').innerHTML =
   new Date().getFullYear());
 
 $(function () {
   var mixer = mixitup('.category__list');
-});
-
-/* Fixed menu*/
-const menu = document.querySelector('.menu');
-
-window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 80) {
-    menu.classList.add('fixed');
-  } else {
-    menu.classList.remove('fixed');
-  }
 });
